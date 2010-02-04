dbae4e670e58c4eb764c18e0cab3ae0d7fcde0db
Tom Crayford <tcrayford@googlemail.com>
2010-02-04 15:42:39 +0000

    version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure_refactoring "0.1.2"
+(defproject clojure_refactoring "0.1.3"
   :description "Clojure side of a refactoring tool"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
