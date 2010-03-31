6919742ea5b378098d2440dfa035ebe5fc34409c
Tom Crayford <tcrayford@googlemail.com>
2010-03-31 17:11:43 +0100

    local-binding-wrap no longer adds another let block

--- src/clojure_refactoring/local_binding.clj
+++ src/clojure_refactoring/local_binding.clj
@@ -1,25 +1,40 @@
 (ns clojure_refactoring.local_binding
   (:use clojure.walk clojure_refactoring.core clojure.contrib.seq-utils))
 
+(defn get-function-definition [defn-form]
+  "Gets the function body out of a defn form"
+  (find-first list? defn-form))
+
+(defn add-to-binding [binding-node value var-name]
+  ""
+  (conj binding-node var-name value))
+
+(defn is-node-the-binding-form? [top-level node]
+  (= node (binding-form top-level)))
+
+(defn modify-existing-let-block [form value var-name]
+  (postwalk
+       (fn [node]
+         (if (is-node-the-binding-form? form node)
+           (add-to-binding node value var-name)
+           node))
+       form))
+
+(defn let-wrap [form value var-name]
+  (if (binding-node? form)
+    (modify-existing-let-block form value var-name)
+    `(~'let [~var-name ~value] ~form)))
+
+(defn wrap-function-body-with-let [defn-form value var-name]
+  (let [fn-def (get-function-definition defn-form)]
+    (postwalk-replace
+     {fn-def (let-wrap fn-def value var-name)} defn-form)))
+
 (defn local-wrap [toplevel value var-name]
   (let [top (read-string toplevel)
         value (read-string value)]
     (format-code
-     (wrap-function-def-with-let
+     (wrap-function-body-with-let
        (postwalk-replace {value (symbol var-name)} top)
        value
        (symbol var-name)))))
-
-(defn wrap-function-def-with-let [defn-form value var-name]
-  (let [fn-def (get-function-definition defn-form)]
-    (postwalk-replace {fn-def `(~'let [~var-name ~value] ~fn-def)} defn-form)))
-
-
-(defn get-function-definition [defn-form]
-  (find-first list? defn-form))
-
-(defn wrap [inside outside]
-  (concat outside (conj '() inside)))
-
-
-
--- test/local_binding_test.clj
+++ test/local_binding_test.clj
@@ -1,5 +1,6 @@
 (ns local_binding_test
-  (:use clojure_refactoring.local_binding clojure.test))
+  (:use clojure.test)
+  (:use clojure_refactoring.local_binding :reload))
 
 (deftest wraps_code_as_local
   (is (= (local-wrap "(defn a [b] (+ b (/ b 1)))"
@@ -7,4 +8,12 @@
                      "c")
          "(defn a [b] (let [c (/ b 1)] (+ b c)))\n")))
 
+(deftest wraps_in_a_local_let_block
+  (is (= (local-wrap "(defn a [b] (let [c 1] (+ b (/ b c))))"
+                     "(/ b c)"
+                     "d")
+         "(defn a [b] (let [c 1 d (/ b c)] (+ b d)))\n")))
 
+(deftest let_wrap
+  (is (= (let-wrap '(let [a 1] a) 1 'b)
+         '(let [a 1 b 1] a))))
