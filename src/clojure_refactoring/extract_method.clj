2a9688ef927af07837408e909f2bc2ff969152e1
Tom Crayford <tcrayford@googlemail.com>
2010-04-17 00:23:16 +0100

    Cleaned up extract method somewhat

--- src/clojure_refactoring/extract_method.clj
+++ src/clojure_refactoring/extract_method.clj
@@ -1,35 +1,43 @@
 (ns clojure_refactoring.extract_method
   (:use clojure_refactoring.core [clojure.contrib str-utils seq-utils]))
 
-(defn fun-call [f-node]
-  "Won't work for multiple arity functions"
-  (conj (for [arg (fn-args f-node)] arg)
-        (nth f-node 1)))
+(defn fn-name [fn-node] (nth fn-node 1))
+
+(defn fn-call [fn-node]
+  "Returns a function call to a function
+using the names in the functions arguments.
+TODO: Won't work for multiple arity functions"
+  (conj (for [arg (fn-args fn-node)] arg) (fn-name fn-node)))
 
 (defn arg-occurences [f-node extracted-node]
-  "Finds the occurrences of bindings from f-node in the extracted node.
-Works for binding forms in core/*binding-forms*"
-  (->>
-   extracted-node
-   (find-occurences (find-bindings f-node extracted-node))
-   (filter #(not= % nil))
-   (unique-vec)))
+  "Finds the bindings from f-node that are also in the extracted node.
+Works for all binding forms in core/binding-forms"
+  (unique-vec
+   (filter
+    #(not= % nil)
+    (find-occurences
+     (find-bindings-above-node f-node extracted-node)
+     extracted-node))))
 
-(defn new-fn [name args body]
-  (conj '() body args (symbol name) 'defn))
+(defn make-fn-node [name args body]
+  `(~'defn ~(symbol name) ~args ~body))
 
-(defn quote-re [s]
+(defn re-quote [s]
   (re-pattern
    (java.util.regex.Pattern/quote s)))
 
+(defn remove-extracted-function [extract-string fn-string new-fun]
+  (re-gsub
+   (re-quote extract-string)
+   (str (fn-call new-fun))
+   fn-string))
+
 (defn format-output [extract-string fn-string new-fun]
+  "Formats the output for extract-method to print"
   (str
-    (format-code new-fun)
-    "\n"
-    (re-gsub
-      (quote-re extract-string)
-      (str (fun-call new-fun))
-      fn-string)))
+   (format-code new-fun)
+   "\n"
+   (remove-extracted-function extract-string fn-string new-fun)))
 
 (defn extract-method [fn-string extract-string new-name]
   "Extracts extract-string out of fn-string and replaces it with a
@@ -38,8 +46,8 @@ function call to the extracted method. Only works on single arity root functions
         extract-node (read-string extract-string)
         args (arg-occurences function-node
                              extract-node)
-        new-fun (new-fn new-name
-                        args
-                        extract-node)]
+        new-fun (make-fn-node new-name
+                              args
+                              extract-node)]
     (format-output extract-string fn-string new-fun)))
 
