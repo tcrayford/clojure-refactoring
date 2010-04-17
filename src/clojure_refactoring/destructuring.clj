c8b687e0963b3eb8c0c2cd56034df5cf739db435
Tom Crayford <tcrayford@googlemail.com>
2010-04-17 15:59:41 +0100

    refactored destructuring somewhat

--- src/clojure_refactoring/destructuring.clj
+++ src/clojure_refactoring/destructuring.clj
@@ -1,68 +1,77 @@
 (ns clojure_refactoring.destructuring
   (:use [clojure.contrib str-utils duck-streams seq-utils pprint] clojure.walk clojure_refactoring.core))
 
-
 (defn count= [seq n]
+  "Checks if the count of seq is equal to n"
   (= (count seq) n))
 
 (defn is-map-lookup?
-  ([n]
-     (and
-      (seq? n)
-      (keyword? (first n))
-      (= (count n) 2)))
-  ([name n]
-     (and
-      (seq? n)
-      (keyword (first n))
-      (= (count n) 2)
-      (= (last n (symbol name))))))
+  "Returns true if node is a map lookup"
+  ([node]
+     (and (seq? node)
+          (or (keyword? (first node)) (keyword? (second node)))
+          (= (count node) 2))))
 
 (defn key->sym [kw]
+  "Turns a keyword into a symbol"
   (symbol (str* kw)))
 
-(defn find-lookups [node]
-  (if (is-map-lookup? node)
-    (into #{} (vector node))
-    (loop [n node accum #{}]
-      (let [current-node (first n)]
-        (if (count= n 0)
-          accum
-          (if (is-map-lookup? current-node)
-            (recur (rest n) (conj accum current-node))
-            (if (seq? current-node)
-              (recur current-node accum)
-              (recur (rest n) accum))))))))
+(defn find-lookups [root-node]
+  "Returns all the map lookups in a node as a set of seqs"
+  (loop [node root-node accum #{}]
+    (let [current-node (first node)]
+      (if (count= node 0)
+        accum
+        (if (is-map-lookup? current-node)
+          (recur (rest node) (conj accum current-node))
+          (if (seq? current-node)
+            (recur current-node accum)
+            (recur (rest node) accum)))))))
+
+(defn lookup->canoninical-form [lookup]
+  "Forces a lookup of the form (map :key) into (:key map)
+TODO: this needs a better name"
+  (if (keyword? (first lookup)) lookup (reverse lookup)))
 
 (defn lookups->binding-map [lookups]
   "Converts #{(:a a) (:b a)} to {a {a :a b :a}}"
-  (loop [l lookups h {}]
+  (loop [l lookups binding-map {}]
     (if (empty? l)
-      h
+      binding-map
       (let [lookup (first l)
-            [key m] lookup]
-        (recur (rest l)
-               (assoc
-                   h
-                 m
-                 (assoc (get h m {})
-                   (key->sym key)
-                   key)))))))
+            [key m] (lookup->canoninical-form lookup)]
+        (recur
+         (rest l)
+         (assoc binding-map m
+                (assoc (get binding-map m {}) (key->sym key) key)))))))
 
 (defn destructured-binding-vec [old-vec binding-map]
+  "Replaces each key in the binding map found in old-vec with the value
+from the binding map"
   (postwalk-replace binding-map old-vec))
 
+(defn replace-lookup-with-destructured-symbol [lookups node]
+  "Replaces node with a destructured symbol from lookups
+if this node is contained in lookups"
+  (if (lookups node)
+    (key->sym (first (lookup->canoninical-form node)))
+    node))
+
+(defn add-binding-map [lookups root-node]
+  "Takes a set of lookups and a function node, and
+adds a binding map made from the lookups to the root node"
+  (map #(if (= % (fn-args root-node))
+          (destructured-binding-vec
+           (fn-args root-node)
+           (lookups->binding-map lookups)) %) root-node))
+
 (defn destructure-map [fn-code name]
-  (let [node (read-string fn-code)
-        lookups (find-lookups node)
-        bnd-map (lookups->binding-map lookups)]
+  "Destructures all calls to map called name inside a function node"
+  (let [root-node (read-string fn-code)
+        lookups (find-lookups root-node)]
     (format-code
      (postwalk
-      (fn [n]
-        (if (lookups n)
-          (key->sym (first n))
-          n))
-      (postwalk-replace
-       (hash-map (fn-args node) (destructured-binding-vec (fn-args node) bnd-map))
-       node)))))
-
+      (partial
+       replace-lookup-with-destructured-symbol
+       lookups)
+      (add-binding-map lookups root-node)))))
--- test/destructuring_test.clj
+++ test/destructuring_test.clj
@@ -3,19 +3,22 @@
 
 (deftest is_map_lookup?
   (is (= (is-map-lookup? '(:foo a)) true))
-  (is (= (is-map-lookup? '(a (:foo b))) false)))
+  (is (= (is-map-lookup? '(a (:foo b))) false))
+  (is (= (is-map-lookup? '(b :foo)) true)))
 
 (deftest find_lookups
-  (is (= (find-lookups '(:foo a)) '#{(:foo a)}))
   (is (= (find-lookups '(a (:foo b))) '#{(:foo b)}))
   (is (= (find-lookups '(defn a [b] (+ (:foo b) (:bar b)))) '#{(:foo b) (:bar b)})))
 
 (deftest lookups->binding_map
   (is (= (lookups->binding-map '#{(:a a)}) '{a {a :a}}))
-  (is (= (lookups->binding-map '#{(:a a) (:b a)}) '{a {a :a b :b}})))
+  (is (= (lookups->binding-map '#{(:a a) (:b a)}) '{a {a :a b :b}}))
+  (is (= (lookups->binding-map '#{(a :a) (a :b)}) '{a {a :a b :b}})))
 
  (deftest destructure_map_test
    (is (= (destructure-map "(defn a [b] (:foo b))" "b")
           "(defn a [{foo :foo}] foo)\n"))
    (is (= (destructure-map "(defn a [b] (+ (:foo b) (:bar b)))" "b")
-          "(defn a [{foo :foo, bar :bar}] (+ foo bar))\n")))
+          "(defn a [{foo :foo, bar :bar}] (+ foo bar))\n"))
+   (is (= (destructure-map "(defn a [b] (+ (b :foo) (b :bar)))" "b")
+          "(defn a [{bar :bar, foo :foo}] (+ foo bar))\n")))
