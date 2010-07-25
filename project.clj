732d0d0dcaa873463358b2b28e7b0c971ba43854
Tom Crayford <tcrayford@googlemail.com>
2010-07-25 21:17:29 +0100

    Updated swank-clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-refactoring "0.1.6-SNAPSHOT"
   :description "Clojure side of a refactoring tool"
-  :dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]]
+  :dependencies [[swank-clojure "1.2.1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
