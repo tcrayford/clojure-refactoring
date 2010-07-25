56cc6570edf171189f470fba40a2b00dcad7d296
Tom Crayford <tcrayford@googlemail.com>
2010-07-25 11:10:47 +0100

    Added todos for multiple defn nodes

--- src/clojure_refactoring/local_binding.clj
+++ src/clojure_refactoring/local_binding.clj
@@ -4,7 +4,8 @@
   (:use [clojure.contrib.seq-utils :only (find-first)]))
 
 (defn get-function-definition [defn-form]
-  "Gets the function body out of a defn form"
+  "Gets the function body out of a defn form
+TODO: doesn't work for multiple arity functions"
   (find-first list? defn-form))
 
 (defn add-to-binding [binding-node value var-name]
@@ -15,11 +16,11 @@
 
 (defn modify-existing-let-block [form value var-name]
   (postwalk
-       (fn [node]
-         (if (is-node-the-binding-form? form node)
-           (add-to-binding node value var-name)
-           node))
-       form))
+   (fn [node]
+     (if (is-node-the-binding-form? form node)
+       (add-to-binding node value var-name)
+       node))
+   form))
 
 (defn let-wrap [form value var-name]
   (if (binding-node? form)
@@ -32,6 +33,7 @@
      {fn-def (let-wrap fn-def value var-name)} defn-form)))
 
 (defn local-wrap [toplevel value var-name]
+  "TODO: only works for defn nodes"
   (let [top (read-string toplevel)
         value (read-string value)]
     (format-code
