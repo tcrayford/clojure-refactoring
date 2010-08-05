acb26908a91f706134b2244dcd27d3b25c5a2bdf
Tom Crayford <tcrayford@googlemail.com>
2010-08-05 03:34:51 +0100

    Moved swank-clojure to dev dependencies.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojure-refactoring "0.1.6-SNAPSHOT"
   :description "Clojure side of a refactoring tool"
-  :dependencies [[swank-clojure "1.2.1"]]
+  :dependencies []
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]
                      [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
