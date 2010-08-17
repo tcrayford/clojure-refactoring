698d8183bf7d2e6dbb47245d6da2ec5a94a41bac
Tom Crayford <tcrayford@googlemail.com>
2010-08-17 19:17:05 +0100

    Doc strings for replace.

--- src/clojure_refactoring/support/replace.clj
+++ src/clojure_refactoring/support/replace.clj
@@ -7,6 +7,7 @@
            (clojure.lang LineNumberingPushbackReader)))
 
 (defn map-to-alist [m]
+  "Converts a clojure map to an alist suitable for emacs"
   (map (fn [[k v]] (list k v)) m))
 
 (def line-from-var (comp :line meta))
@@ -15,6 +16,8 @@
   (slime-find-file (file-from-var v)))
 
 (defn build-replacement-map [v f]
+  "Builds a replacement for a var (to be sent to emacs) by calling f on the parsley tree
+from that var."
   {:file (slime-file-from-var v)
    :var-name (.sym v)
    :line (line-from-var v)
@@ -22,7 +25,9 @@
                 (f (:parsley (get-entry-from-cache v))))})
 
 (defn replace-vars [vars f]
+  "Replaces vars by calling f on each one."
   (map #(map-to-alist (build-replacement-map % f)) vars))
 
 (defn replace-callers [v f]
+  "Replaces all callers of a var by calling a function on them."
   (replace-vars (vars-who-call v) f))
