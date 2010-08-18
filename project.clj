c07865ebaf7405ebd63a6802da0890f968c2879d
Tom Crayford <tcrayford@googlemail.com>
2010-08-18 19:14:43 +0100

    Added radagast.
    Run using:
    
    lein radagast clojure-refactoring.destructuring-test clojure-refactoring.extract-method-test clojure-refactoring.local-binding-test clojure-refactoring.rename-test clojure-refactoring.thread-expression-test clojure-refactoring.support.core-test clojure-refactoring.support.find-bindings-above-node-test clojure-refactoring.support.namespaces-test clojure-refactoring.support.parsley-test clojure-refactoring.support.replace-test clojure-refactoring.support.source-test

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
                  [tcrayford_parsley/parsley "0.1.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [radagast "1.0.0"]])
